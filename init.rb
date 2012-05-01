eabd6424da48beb17d71fb76be9cba38ca3c90b4
William Roush <william.roush@roushtech.net>
2012-05-01 02:12:05 -0400

    Redmine 1.4.0 compatability.

--- init.rb
+++ init.rb
@@ -11,7 +11,7 @@ Redmine::Plugin.register :redmine_attach_screenshot do
   author 'Konstantin Zaitsev, Sergei Vasiliev, Alexandr Poplavsky, Axmor Software, Jens Alfke, Renzo Meister, William Roush'
   author_url 'https://bitbucket.org/StrangeWill/redmine-inline-attach-screenshot/'
   description 'Attach screenshots from clipboard directly to a Redmine issue.'
-  version '0.2.0'
+  version '0.3.0'
   Dispatcher.to_prepare do
     ApplicationController.send(:include, AttachScreenshotPlugin::ApplicationControllerPatch)
     AccountController.send(:include, AttachScreenshotPlugin::CleanupTmp)

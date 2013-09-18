a43bf3ed8ad1925dfc4ad88912e651347c5641d8
William Roush <william.roush@roushtech.net>
2013-09-18 00:25:16 -0400

    Fixes mutliple file upload in Redmine 2.3.x

--- init.rb
+++ init.rb
@@ -9,10 +9,10 @@ end
 Redmine::Plugin.register :redmine_inline_attach_screenshot do
   name 'Redmine Attach Screenshot plugin'
   url 'https://bitbucket.org/StrangeWill/redmine-inline-attach-screenshot/'
-  author 'Konstantin Zaitsev, Sergei Vasiliev, Alexandr Poplavsky, Axmor Software, Jens Alfke, Renzo Meister, William Roush'
+  author 'Konstantin Zaitsev, Sergei Vasiliev, Alexandr Poplavsky, Axmor Software, Jens Alfke, Renzo Meister, YongKi Kim, William Roush'
   author_url 'https://bitbucket.org/StrangeWill/redmine-inline-attach-screenshot/'
   description 'Attach screenshots from clipboard directly to a Redmine issue.'
-  version '0.4.2'
+  version '0.4.6'
   
   if Gem::Version.new("3.0") > Gem::Version.new(Rails.version) then
     Dispatcher.to_prepare do
